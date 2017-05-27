837d02eb2679a76a7a1a26afd9722a9df2eff50e
Ela Yudhanira <elayudhanira@Elas-MacBook-Air.local>
2017-05-28 00:07:38 +0200

    Version 1.0

--- dist/js/main/all.js
+++ dist/js/main/all.js
@@ -2,6 +2,27 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['intro', 'brainstorming', 'explore','experiment','final'],
     navigation: true,
-    navigationPosition: 'right'
+    navigationPosition: 'right',
+    slidesNavPosition: 'center',
+    verticalCentered: false
+  });
+
+  $('.js-cp').click(function(e){
+    e.preventDefault();
+    var href = $(this).attr('href');
+    $(href).addClass('active');
+  });
+  $('.js-intro').click(function(e){
+    e.preventDefault();
+    $('#introanim').toggleClass('active');
+  });
+  $('.wb-close').click(function(e){
+    e.preventDefault();
+    $(this).closest('.wb-modal').removeClass('active');
+  });
+
+  $('.to-sec-4').click(function(e){
+    e.preventDefault();
+    $('.sec-4').toggleClass('active');
   });
 });
