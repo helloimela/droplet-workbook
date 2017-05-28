2d45e37c2820d24319d0666bc56844c77534b81e
Ela Yudhanira <elayudhanira@n146-p41.kthopen.kth.se>
2017-05-28 19:55:39 +0200

    Update sketches

--- dist/js/main/all.js
+++ dist/js/main/all.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['intro', 'brainstorming', 'explore','experiment','final'],
+    anchors: ['intro', 'brainstorming', 'explore','experiment','final','cheers'],
     navigation: true,
     navigationPosition: 'right',
     slidesNavPosition: 'center',
@@ -25,4 +25,34 @@ $(document).ready(function() {
     e.preventDefault();
     $('.sec-4').toggleClass('active');
   });
+
+  var slideWidth = $('.wb-slide').width();
+  $('.wb-modal .wb-slide ul li').css({ width: slideWidth});
+
+  function moveLeft() {
+      $('.wb-slide ul').animate({
+          left: + slideWidth
+      }, 200, function () {
+          $('.wb-slide ul li:last-child').prependTo('.wb-slide ul');
+          $('.wb-slide ul').css('left', '');
+      });
+  };
+
+  function moveRight() {
+      $('.wb-slide ul').animate({
+          left: - slideWidth
+      }, 200, function () {
+          $('.wb-slide ul li:first-child').appendTo('.wb-slide ul');
+          $('.wb-slide ul').css('left', '');
+      });
+  };
+
+  $('a.control-prev').click(function () {
+      moveLeft();
+  });
+
+  $('a.control-next').click(function () {
+      moveRight();
+  });
+
 });
